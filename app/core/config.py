b5780c5afd8122ee5391396eea03ff4f5ca0145f
better.call.saro <bssaravanan247@gmail.com>
2025-11-09 22:54:03 +0530

    dumb me

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,20 @@
 import os
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql+asyncpg://saravanan:saravanan24@localhost/genchargetesting")
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "sivaji_vaila_jelabi")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1
     REFRESH_TOKEN_EXPIRE_DAYS: int = 2
     OTP_EXPIRE_MINUTES: int = 5
-    OTP_SECRET: str = os.getenv("OTP_SECRET", "pooriya_pongala")
-    ALGORITHM: str = "HS256"
-    MONGO_URL: str = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-    MONGO_DB_NAME: str  = os.getenv("MONGO_DB_NAME", "genchargetesting")
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "saravanan")
-    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "saravanan24")
+    OTP_SECRET: str = os.getenv("OTP_SECRET")
+    ALGORITHM: str = os.getenv("ALGORITHM")
+    MONGO_URL: str = os.getenv("MONGO_URL")
+    MONGO_DB_NAME: str  = os.getenv("MONGO_DB_NAME")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
 
 settings = Settings()
